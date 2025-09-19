547f9b7018050bd4cfdd78bbd11b535a5d4e327e
Malak Elkassas <elkassasmalak24@gmail.com>
2025-09-19 12:16:49 +0300

    Add files via upload

--- src/ChildrenManagement/ChildrenManagement.jsx
+++ src/ChildrenManagement/ChildrenManagement.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-export default function ChildrenManagement() {
+function ChildManagementHeader() {
   return (
-    <div>
-      
-    </div>
-  )
+    <div className="w-full min-h-full bg-white border  border-[#E4E7EC] rounded-lg">
+  <div className="flex justify-between items-center p-4">
+    <h1 className="text-right text-gray-800 text-2xl font-medium">
+      إدارة الأطفال
+    </h1>
+    <button className="bg-[#007AFF] text-white text-sm px-4 py-2 rounded shadow-md active:shadow-inner transform active:translate-y-[1px] transition">
+  + إضافة طفل جديد
+</button>
+
+  </div>
+  <div className="border-b border-gray-200 w-full"></div>
+</div>
+  );
 }
+
+export default ChildManagementHeader;
